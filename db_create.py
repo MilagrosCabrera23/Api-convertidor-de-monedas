3990e39161eb34d13ee56967a0845424422c0784
MilagrosCabrera <maguicabrera23@gmail.com>
2025-07-22 08:50:17 -0300

    Agrego main.py y db_create.py

--- db_create.py
+++ db_create.py
@@ -1,5 +1,6 @@
-from app.database.db import Base, engine
-from app.models.user import User 
+from app.database.bd import Base, engine
+from app.models.users import User 
+from app.models.convert import Convert
 
 print("Creando las tablas en la base de datos...")
 Base.metadata.create_all(bind=engine)
