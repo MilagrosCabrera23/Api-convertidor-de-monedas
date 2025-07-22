9f5ebfdeebdadeec619d02adf0eeb035519f6ca7
MilagrosCabrera <maguicabrera23@gmail.com>
2025-07-22 10:32:56 -0300

    Eliminar carpetas __pycache__ del repositorio

--- db_create.py
+++
@@ -1,7 +0,0 @@
-from app.database.bd import Base, engine
-from app.models.users import User 
-from app.models.convert import Convert
-
-print("Creando las tablas en la base de datos...")
-Base.metadata.create_all(bind=engine)
-print("¡Tablas creadas con éxito!")
